1dc1e521998199c57459503a8a2f8e9f37fbfc3e
cvukxwho <chienvuk@gmail.com>
2023-12-16 10:19:52 +0700

    succesfully push to local

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
+import UserModel from "./Models/user.js";
 
 mongoose.connect("mongodb://localhost:27017");
 
 const app = express();
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("Welcome");
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const listUser = await UserModel.find();
+    res.status(200).send({
+      message: "Success!",
+      data: listUser,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
 });
 
 app.listen(3001, () => {
